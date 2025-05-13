8bf29eb4577f93740d2678f68f5e91f6d75feea3
“ksj686” <kim_se_joon@naver.com>
2025-05-13 14:52:32 +0900

    필요없는 파일 삭제

--- src/store/mediaDeviceStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useMediaDeviceStore = create((set) => ({
-  selectedMicId: null,
-  selectedCameraId: null,
-  setMicId: (str) => set({ selectedMicId: str }),
-  setCameraId: (str) => set({ selectedCameraId: str }),
-  resetDevices: () => set({ selectedMicId: null, selectedCameraId: null }),
-}));
